3ec48ddd56162cc219c002d0c11a350c3dd591c6
Sigrid <sigridxyx@gmail.com>
2020-04-23 20:34:27 +0800

    Update translate.sh

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -4,18 +4,18 @@ set -x
 
 DATAHOME=${HOME}/datasets
 EXEHOME=${HOME}/src
-MODELHOME=${HOME}/models/generator
-LOGHOME=${HOME}/logs/predictions
+MODELHOME=${HOME}/models
+LOGHOME=${HOME}/predictions
 
 mkdir -p ${LOGHOME}
 
 cd ${EXEHOME}
 
 python translate.py \
-       -model ${DATAHOME}/generator_15.12441_bleu4.chkpt \
-       -sequence_data ${DATAHOME}/preprcessed_sequence_data.pt \
-       -graph_data ${DATAHOME}/preprcessed_graph_data.pt \
-       -valid_data ${DATAHOME}/Dataset/valid_dataset.pt \
+       -model ${MODELHOME}/generator_15.12441_bleu4.chkpt \
+       -sequence_data ${DATAHOME}/preprocessed-data/preprcessed_sequence_data.pt \
+       -graph_data ${DATAHOME}/preprocessed-data/preprcessed_graph_data.pt \
+       -valid_data ${DATAHOME}/Datasets/valid_dataset.pt \
        -output ${LOGHOME}/prediction.txt \
        -beam_size 5 \
        -batch_size 16 \
